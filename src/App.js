ccb7bb1207d9af7ef6ce94d23ee98515623cbd66
Sami Ullah <muftisamiullah@localhost.localdomain>
2020-06-06 10:15:34 +0530

    test up & added build files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Website under Construction :)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://g33kzd3n.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          know more
         </a>
       </header>
     </div>
