8ac7186dee628c9785e61bf3a73d79b42130f135
Basit Mir <iammirbasit@gmail.com>
2020-06-09 14:13:28 +0530

    navbar fixes complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,135 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import $ from "jquery";
+// import * as Scroll from "react-scroll";
+import { Link } from "react-scroll";
 const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+  const [openNav, setOpenNav] = useState("menu-trigger");
+  const [subMenu, setSubMenu] = useState(null);
+  const mobileNavOpen = useRef(null);
+
   useEffect(() => {
-    if ($(".menu-trigger").length) {
-      $(".menu-trigger").on("click", function () {
-        $(this).toggleClass("active");
-        $(".header-area .nav").slideToggle(200);
-      });
-    }
-    $(window).scroll(function () {
-      var scroll = $(window).scrollTop();
-      var box = $(".header-text").height();
-      var header = $("header").height();
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
-      if (scroll >= box - header) {
-        $("header").addClass("background-header");
-      } else {
-        $("header").removeClass("background-header");
-      }
-    });
-
-    // Window Resize Mobile Menu Fix
-    mobileNav();
-
-    $('a[href^="#welcome"]').addClass("active");
-
-    //smoothscroll
-    $(".menu-item").on("click", function (e) {
-      e.preventDefault();
-      var athis = this;
-      var target = this.hash,
-        menu = target;
-      var $target = $(target);
-
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $target.offset().top,
-          },
-          500,
-          "swing",
-          function () {
-            window.location.hash = target;
-            $(".menu-item").removeClass("active");
-            $(athis).addClass("active");
-          }
-        );
-
-      $(".main-nav > .nav").css("display", "none");
-      $(".menu-trigger").removeClass("active");
-    });
-
-    $(window).scroll(function (event) {
-      var scrollPos = $(document).scrollTop() + 80;
-
-      if (scrollPos === 0) {
-        $('a[href^="#welcome"]').addClass("active");
-        return;
-      }
-      $(".menu-item")
-        .not('[href="#none"]')
-        .not('[href="#dropdown"]')
-        .each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-
-          if (
-            refElement.position().top <= scrollPos &&
-            refElement.position().top + refElement.height() > scrollPos
-          ) {
-            $(".menu-item").removeClass("active");
-            currLink.addClass("active");
-          } else {
-            currLink.removeClass("active");
-          }
-        });
-    });
-
-    // Window Resize Mobile Menu Fix
-    $(window).on("resize", function () {
-      mobileNav();
-    });
-
-    // Window Resize Mobile Menu Fix
-    function mobileNav() {
-      var width = $(window).width();
-      $(".submenu").on("click", function () {
-        if (width < 992) {
-          $(".submenu ul").removeClass("active");
-          $(this).find("ul").toggleClass("active");
-        }
-      });
-    }
-
-    // Menu elevator animation
-    $("a[href*=\\#dropdown]:not([href=\\#none])").on("click", function () {
-      if (
-        window.location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        window.location.hostname == this.hostname
-      ) {
-        var targetHash = this.hash;
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          var width = $(window).width();
-          if (width < 991) {
-            $(".menu-trigger").removeClass("active");
-            $(".header-area .nav").slideUp(200);
-          }
-          $("html,body").animate(
-            {
-              scrollTop: target.offset().top,
-            },
-            700,
-            "swing",
-            function () {
-              window.location.hash = targetHash;
-            }
-          );
-          return false;
-        }
-      }
-    });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
+  const handleMobileNav = () => {
+    if (openNav === "menu-trigger") {
+      setOpenNav("menu-trigger active");
+    } else {
+      setOpenNav("menu-trigger");
+    }
+    $(".header-area .nav").slideToggle(200);
+  };
+
+  const closeNavBar = () => {
+    setOpenNav("menu-trigger");
+    mobileNavOpen.current.style.display = "none";
+  };
+
+  const handleDropDown = () => {
+    if (subMenu === null) {
+      setSubMenu("active");
+    } else {
+      setSubMenu(null);
+    }
+  };
   return (
-    <header className="header-area header-sticky">
+    <header
+      className={
+        scrollPosition < 646
+          ? "header-area header-sticky"
+          : "header-area header-sticky background-header"
+      }
+    >
       <div className="container">
         <div className="row">
           <div className="col-12">
@@ -140,25 +59,70 @@ const Navbar = () => {
               </a>
               {/* <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** --> */}
-              <ul className="nav">
+              <ul className="nav" ref={mobileNavOpen}>
                 <li className="scroll-to-section">
-                  <a href="#welcome" className="menu-item">
+                  <Link
+                    onClick={closeNavBar}
+                    style={{ cursor: "pointer" }}
+                    className="menu-item"
+                    activeClass="active"
+                    to="welcome"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    // isDynamic={true}
+                  >
                     Home
-                  </a>
+                  </Link>
+                  {/* <a href="#welcome" className="menu-item">
+                    Home
+                  </a> */}
                 </li>
                 <li className="scroll-to-section">
-                  <a href="#about" className="menu-item">
+                  <Link
+                    onClick={closeNavBar}
+                    style={{ cursor: "pointer" }}
+                    className="menu-item"
+                    activeClass="active"
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    // isDynamic={true}
+                  >
                     About
-                  </a>
+                  </Link>
+                  {/* <a href="#about" className="menu-item">
+                    About
+                  </a> */}
                 </li>
                 <li className="scroll-to-section">
-                  <a href="#testimonials" className="menu-item">
+                  <Link
+                    onClick={closeNavBar}
+                    style={{ cursor: "pointer" }}
+                    className="menu-item"
+                    activeClass="active"
+                    to="testimonials"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    // isDynamic={true}
+                  >
                     Testimonials
-                  </a>
+                  </Link>
+                  {/* <a href="#testimonials" className="menu-item">
+                    Testimonials
+                  </a> */}
                 </li>
-                <li className="submenu">
-                  <a href="#dropdown">Drop Down</a>
-                  <ul>
+                <li
+                  className="submenu"
+                  style={{ borderWidth: 1, borderColor: "#eee" }}
+                >
+                  <a href="#dropdown" onClick={handleDropDown}>
+                    Drop Down
+                  </a>
+
+                  <ul className={subMenu}>
                     <li>
                       <a href="#none" className="menu-item">
                         About Us
@@ -182,12 +146,25 @@ const Navbar = () => {
                   </ul>
                 </li>
                 <li className="scroll-to-section">
-                  <a href="#contact-us" className="menu-item">
+                  <Link
+                    onClick={closeNavBar}
+                    style={{ cursor: "pointer" }}
+                    className="menu-item"
+                    activeClass="active"
+                    to="contact-us"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    // isDynamic={true}
+                  >
                     Contact Us
-                  </a>
+                  </Link>
+                  {/* <a href="#contact-us" className="menu-item">
+                    Contact Us
+                  </a> */}
                 </li>
               </ul>
-              <a href="#none" className="menu-trigger">
+              <a href="#none" className={openNav} onClick={handleMobileNav}>
                 <span>Menu</span>
               </a>
               {/* <!-- ***** Menu End ***** --> */}
